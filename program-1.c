f2b5025177e49aaa1a88ce35d3d701a66cade0f3
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-20 15:04:19 +0545

    Update program-1.c
    add two numbers

---
+++ program-1.c
@@ -0,0 +1,13 @@
+// Add two numbers
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("enter the value of  num1");
+    scanf("%d",&num1);
+    printf("enter the value of num2");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("value of num3 is %d",sum);
+    return(0);
+}
