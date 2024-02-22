d5dfbb39733ce39232742d548ca35efa698aa503
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-22 09:50:32 +0545

    Add files via upload
    Armstrong numbers.c

---
+++ Armstrong numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i, sum,temp;
+	for(i=1;i<=999;i++)
+	{
+		sum=0;
+		temp=i;
+		while(temp!=0)
+		{
+			int ext;
+			ext=temp%10;
+			sum=sum+(ext*ext*ext);
+			temp=temp/10;
+			
+		}
+		if(i==sum)
+		{
+			printf("%d\t",i);
+		}
+	}
+	
+	
+}
+
