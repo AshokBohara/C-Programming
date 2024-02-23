13a9f6e940e482eda36f276df36de7bf3ca1b7a1
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-23 07:16:51 +0545

    Add files via upload

---
+++ STRING TYPE FULL NAME.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char name [100];//array of character
+	printf("enter a string");
+	scanf("%s",name);
+	printf("name=%s",name);
+	gets (name);
+	puts("name=");
+	puts(name);
+	}
