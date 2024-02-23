b24b732cafb240fd2d59246de0de85693b347ba6
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-23 07:21:00 +0545

    Add files via upload

---
+++ 2number bet^n odd nums.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("Enter the value of n1 and n2");
+	scanf("%d%d",&n1,&n2);
+	if(n1<n2)
+	{
+	for(i=n1;i<=n2;i=i++);
+	{
+	if(%n2==0)
+	{
+	printf("%d\t",i);
+	}
+	}
+	}
+	else
+	{
+	printf("n1 must be less than n2");
+	}
+}
