f2559ea6a099335d2ea3e6b9f5b264b90d1693c7
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-24 23:40:51 +0545

    Add files via upload

---
+++ types of triangle.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c
+	printf("enter_three_side_of_triangle")
+	scanf("%f%f%f",&a,&b,&c);
+	if((a+b>c)&&(b+c>a)&&(c+a>b))
+	{
+    if(a==b&b==c)
+    {
+    printf("the triangle is equilateral");
+    {
+    else
+    if (a==b||b==c||a==c)
+    {
+    printf("the triangle is isoceles");
+    {
+   	else
+   	{
+   	printf("the triangle is scelence");
+   	{
+   	else	
+	   }
+   	printf("the triangle is invalid");
+   	return(0);
+   }
+	}		
+	}
+	}
+}
