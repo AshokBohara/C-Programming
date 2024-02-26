c4197ff6329e1d54955d06b12d2f32f4944ba064
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-26 22:35:38 +0545

    Add files via upload
    self

---
+++ Untitled3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    printf("enter the value of  num1");
+    scanf("%d",&num1);
+    printf("enter the value of num2");
+    scanf("%d",&num2);
+    num3=num1+num2;
+    printf("value of num3 is %d",num3);
+    return(0);
+}
