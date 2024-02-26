2ce8f3984d582809027d153fa853fbd0dc0044b9
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-26 22:34:38 +0545

    Add files via upload

---
+++ structure and uinon.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct Employee
+{
+ char name[100];
+ char address[100];
+ char post[100];
+ float salary;
+};
+int main()
+{
+	struct Employee e;
+	printf("enter name address post and salary of employee\n");
+	scanf("%s%s%s%f",e.name,e.address,e.post,e.salary);
+	printf("name=%s\n",e.name);
+	printf("address=%s\n",e.address);
+	printf("post=%s\n",e.post);
+	printf("salary=%f\n",e.salary);
+}
