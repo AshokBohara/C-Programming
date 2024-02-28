7c82ef9ee2c92351142c04b43510c0ad8b47ac10
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-28 22:15:32 +0545

    Add files via upload

---
+++ area n cir of circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float rad,area,cir,PI=3.14;
+	printf("enter a rad");
+	scanf("%f",&rad);
+	area=3.14*pow(rad,2);
+	cir=2*3.14*rad;
+	printf("area is %f\n",area);
+	printf("circumference is %f",cir);
+	
+}
