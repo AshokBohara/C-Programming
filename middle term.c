aeec40207e73275657b972683f3a029a4beb0e5d
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-02-29 09:42:49 +0545

    Add files via upload

---
+++ middle term.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,num3;
+	printf("enter three numbers ");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	if((num1>num2 && num2>num3) || (num1<num2&&num2<num3))
+	{
+		printf("%d is middle term",num2);	
+	}
+     else
+     if((num2>num1&&num1>num3) || (num2<num1&&num1<num3))
+     {
+     	printf("%d is middle term",num1);
+     	
+	 }
+	  else
+	  printf("%d is middle term",num3);
+	  
+
+}
