5dc507bb94bb7b7c01a57e63d42abd1d7a0b4e65
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-03-03 20:47:21 +0545

    Add files via upload

---
+++ function type.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+float getArea(float,float);
+float getperimeter(float,float);
+int main()
+{
+	float len,bre,area,peri;
+	printf("enter length and breadth");
+	scanf("%f%f",&len,&bre);
+	area=getarea(len,bre);
+	peri=getperi(len,bre);
+	printf("area of rectangle=%f",area);
+	printf("perimeter of rectangle=%f",peri);
+	return(0);
+	}
+	float getArea(float len,float bre)
+	{
+	return(len*bre);
+	}	
+	float getperimeter(float len,float bre)
+	{
+return(2*(len+bre));
+}
