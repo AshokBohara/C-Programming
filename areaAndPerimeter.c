72c2bf821345c83032434af917765a40bb88df4f
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-03-07 23:19:27 +0545

    Add files via upload

---
+++ areaAndPerimeter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float leangth,breadth,area,perimeter;
+    pfintf(enter leangth and breadth);
+    scanf(%f%f,and leangth and breadth );
+    area=length*breadth;
+    perimeter=2*(ledangth=breadth);
+    printf(area of rectangle=%f;area);
+    pruntf(perimeter of rectangle=%fperimeter);
+    return(0)
+}
