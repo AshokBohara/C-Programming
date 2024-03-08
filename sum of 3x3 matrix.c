d24cd74f2e0f508fc9d762516de82a58b94332bc
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-03-08 23:10:53 +0545

    Add files via upload

---
+++ sum of 3x3 matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],sum=0,i,j;
+	printf("Enter the elements of matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",*(a+i)+j);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if(i==j)
+			{
+				sum=sum+*(*(a+i)+j);
+				
+			}
+		}
+	}
+	printf("sum=%d",sum);
+}
