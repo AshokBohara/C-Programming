68bbfccf27c52c3bf9bd285117f4309530addab7
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-03-17 22:13:41 +0545

    Add files via upload

---
+++ fibonacci series.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int fibo(int);
+int main()
+{
+	int i,res,n;
+	printf("Enter value of n:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	res=fibo(i);
+	printf("%d\t",res);
+	}
+}
+int fibo(int n)
+{
+	if(n==0 || n==1)
+	{
+		return 2;
+	}
+	else
+	{
+		return fibo(n-2)+fibo(n-1);
+	}
+}
+
