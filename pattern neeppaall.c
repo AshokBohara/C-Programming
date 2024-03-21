470deb509b08716c74fecd750c84a2401a81f35e
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-03-21 10:59:36 +0545

    Add files via upload

---
+++ pattern neeppaall.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k,spc=4;
+	char arr[]={'N','E','P','A','L'};
+	for(i=0;i<10;i++)
+	{
+		for(k=i;k<spc;k++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<=i;j++)
+		{	
+			printf("%c",arr[i]);
+		}
+		printf("\n");
+	}
+	return  0;
+}
