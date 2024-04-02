5998afaae87dc42630db77519f0855bbd9b52650
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-04-02 20:34:33 +0545

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int swap(int*,int*);
+int main()
+{
+	int num1=4,num2=5;
+	printf("enter two numbers\n");
+	scanf("%d%d",&num1,&num2);
+    printf("before swapping\n");
+    printf("num1= %d\n",num1);
+    printf("num2=%d\n",num2);
+    swap(&num1,&num2);
+    printf("after swapping\n");
+    printf("num1= %d\n",num1);
+    printf(" mum2= %d\n",num2);   
+}
+int swap(int*x,int*y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+	
+}
