4d3efd6229943a2e1d9f0b4262cc5814c55ada95
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-04-10 20:55:42 +0545

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n[5],temp;
+	printf("enter five number\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&n[i]);	
+	}
+    for(i=0;i<5;i++)
+	{
+		for(j=i+1;j<5;j++)
+	{
+	if(n[i]<n[j])
+	 }
+	}
+
+		printf("shorted value");
+		for(i=0;i<5;i++)
+		{
+			printf("%d\t",n[i]);
+}
+}
+
+
+
