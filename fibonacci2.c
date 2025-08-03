74f71ec7390b83e08a4dd7f706841f8987b67887
Ashok Bohara <boharaashok32@gmail.com>
2025-08-04 01:13:00 +0545

    fiboSEris

--- fibonacci2.c
+++ fibonacci2.c
@@ -2,9 +2,13 @@
 #include<stdio.h>
 int main() {
     int i, n = 8, t1 = 0, t2 = 1, t3, temp;
+
     printf("%d\t%d\t", t1, t2);
-    for(i = 3; i <= n; i++) {
+
+    for(i = 3; i <= n; i++)
+    {
         t3 = t1 + t2;
+        
         printf("%d\t", t3);
         t1 = t2;
         t2 = t3;
