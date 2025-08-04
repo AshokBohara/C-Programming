0958f01a7957cd7d741751a05b53387c2eb5b1d9
Ashok Bohara <boharaashok32@gmail.com>
2025-08-04 22:59:17 +0545

    modified

--- structure and uinon.c
+++ structure and uinon.c
@@ -1,18 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+
 struct Employee
 {
- char name[100];
- char address[100];
- char post[100];
- float salary;
+    char name[100];
+    char address[100];
+    char post[100];
+    float salary;
 };
+
 int main()
 {
-	struct Employee e;
-	printf("enter name address post and salary of employee\n");
-	scanf("%s%s%s%f",e.name,e.address,e.post,e.salary);
-	printf("name=%s\n",e.name);
-	printf("address=%s\n",e.address);
-	printf("post=%s\n",e.post);
-	printf("salary=%f\n",e.salary);
+    struct Employee e;
+
+    printf("Enter name, address, post and salary of employee:\n");
+    scanf("%s %s %s %f", e.name, e.address, e.post, &e.salary);
+
+    printf("Name = %s\n", e.name);
+    printf("Address = %s\n", e.address);
+    printf("Post = %s\n", e.post);
+    printf("Salary = %.2f\n", e.salary);
+
+    return 0;
 }
